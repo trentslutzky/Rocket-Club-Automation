493e667928c024ce4e852aa68ef6865cdf40a418
Trent Slutzky <trent.slutzky@gmail.com>
2020-07-30 18:30:37 -0400

    updated delays

--- RCData.py
+++ RCData.py
@@ -112,7 +112,7 @@ sheets_service = auth_sheets() # store sheets authorization
 sheet = sheets_service.spreadsheets()
 
 base_delay = 1
-base_delay_site = 1
+base_delay_site = .1
 error_delay = 5
 
 def get_cells(sheet_id, newRange):
