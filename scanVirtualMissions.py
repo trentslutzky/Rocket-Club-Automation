a2332b7832a0e1603713456136b5c93962d6a846
Trent Slutzky <trent.slutzky@gmail.com>
2020-08-03 16:22:06 -0400

    progress bars on stats page

--- scanVirtualMissions.py
+++ scanVirtualMissions.py
@@ -9,6 +9,7 @@ from colorama import Fore, Back, Style
 coding_vm_folder_id = "'1SG63RmmC42yQ_eR_-dq9IPfWC83DsRbg'"
 ########## Virtual Missions Main Sheet ID ##########
 vm_master_sheet_id = '1rVblBZu9FXzM9guC9R5CxQEtYlTN_NGfFiv2X4ivh70'
+member_stats_sheet_id = '1UkJKMY735oTSohu8X5eM_rvFp267KBbaEiVg4SXaki8'
 ####################################################
 
 def parse_score(scoreString):
@@ -16,10 +17,12 @@ def parse_score(scoreString):
     score = int(scoreSplit[0])
     return score
 
-def scan_folder(keyword,folder_id):
+def scan_folder(keyword,folder_id,num_column):
     print(Fore.BLUE + 'Scanning ' + Fore.GREEN + keyword)
     print(Fore.GREEN + 'Fetch master VM sheet -> ', end = ' ')
     vm_master_data = rcdata.get_cells(vm_master_sheet_id,'A2:1000')
+    member_stats_data = rcdata.get_cells(member_stats_sheet_id,'A2:1000')
+    vm_num_data = rcdata.get_cells(member_stats_sheet_id,'vm_nums')
     score_col = vm_master_data[0].index(keyword)
     id_col = vm_master_data[0].index('member_id')
     new_q = folder_id + " in parents"
@@ -33,7 +36,8 @@ def scan_folder(keyword,folder_id):
         sheet_name = sheet['name']
         if(sheet_name != 'Unused'):
             scans += 1
-            print(Fore.BLUE + str(scans) + Fore.WHITE + ' - Scanning ' + Fore.GREEN + sheet_name[:40] + '...')
+            print(Fore.BLUE + str(scans) + Fore.WHITE + ' - Scanning ' 
+                    + Fore.GREEN + sheet_name[:40] + '...')
             print('    ', end='')
             current_sheet = rcdata.get_cells(sheet['id'],'A1:1000')
             id_index = current_sheet[0].index('Member ID Number ')
@@ -45,11 +49,14 @@ def scan_folder(keyword,folder_id):
                     if('#' not in member_id):
                         sheet_needs_updating = True
                         score = parse_score(str(row[score_index]))
-                        print('    ' + Fore.YELLOW + member_id + Fore.WHITE + ' got ' + Fore.YELLOW + str(score))
+                        print('    ' + Fore.YELLOW + member_id + Fore.WHITE 
+                                + ' got ' + Fore.YELLOW + str(score))
                         for master_row in vm_master_data:
                             if master_row[id_col] == member_id:
                                 master_row[score_col] = str(int(master_row[score_col]) + score)
                         row[id_index] = '#' + row[id_index]
+                        # update number of vms completed in member stats:
+                        vm_num_data = add_vm_totals(member_stats_data,vm_num_data,member_id,num_column)
             if sheet_needs_updating:
                 print(Fore.WHITE + '    Updating sheet -> ', end = ' ')
                 rcdata.set_cells(sheet['id'],'A1:1000',current_sheet)
@@ -58,6 +65,17 @@ def scan_folder(keyword,folder_id):
     print(Fore.BLUE + 'Finished Scanning ' + Fore.GREEN + keyword + '!')
     print(Fore.GREEN + 'Updating Master Sheet -> ',end = '')
     rcdata.set_cells(vm_master_sheet_id,'A2:1000',vm_master_data)
+    rcdata.set_cells(member_stats_sheet_id,'vm_nums',vm_num_data)
+
+# Search for member id in member_stats sheet then use index to sed number in vm_nums range
+def add_vm_totals(member_stats_data,num_data,member_id,num_column):
+    for member in member_stats_data:
+        if member[0] == str(member_id):
+            member_index = member_stats_data.index(member)
+    column_index = num_data[0].index(num_column)
+    num_data[member_index][column_index] = str(int(num_data[member_index][column_index])+1)
+    return num_data
+
 
 def do_total_column():
     print(Fore.GREEN + 'Calculating Totals -> ',end = '')
@@ -75,8 +93,33 @@ def main():
     print(Fore.BLUE + '#####################################')
     print(Fore.BLUE + '###   Updating Virtual Missions   ###')
     print(Fore.BLUE + '#####################################')
-    scan_folder('vm_coding',coding_vm_folder_id)
+    scan_folder('vm_coding_overview',coding_vm_folder_id,'num_coding_overview')
     do_total_column()
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
