7985ed55ae73c45f3cabb974f5add1ea0d86eb33
Trent Slutzky <trent.slutzky@gmail.com>
2021-02-02 15:16:30 -0500

    added temp page for rc_parents

--- RC_Parents/rc_parents.py
+++ RC_Parents/rc_parents.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return render_template('welcome.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
